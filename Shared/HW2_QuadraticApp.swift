fd9190ef1266f9b492861f0277a84c4ccc359c48
Daksh <dpatel132@hawk.iit.edu>
2022-01-28 12:43:30 -0600

    Initial Commit

---
+++ Shared/HW2_QuadraticApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW2_QuadraticApp.swift
+//  Shared
+//
+//  Created by Daksh Patel on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct HW2_QuadraticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
