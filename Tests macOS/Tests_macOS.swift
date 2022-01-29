42c4c7e2cfa0775a43cece79e20f2c159abddf08
Daksh <dpatel132@hawk.iit.edu>
2022-01-29 14:27:21 -0600

    added test

--- Tests macOS/Tests_macOS.swift
+++ Tests macOS/Tests_macOS.swift
@@ -30,6 +30,56 @@ class Tests_macOS: XCTestCase {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+    
+    func testForm1Solution1() async{
+       
+        let a = 3.0
+        let b = 5.0
+        let c = -2.0
+        let quad = Quadratic()
+        
+        let form1sol1 = quad.quadraticForm1Sol1(aValue: a, bValue: b, cValue: c)
+        
+        XCTAssertEqual(form1sol1, ((1/3)), accuracy: 1.0E-7, "was not equal to this resolution")
+    }
+    
+    func testForm1Solution2() async{
+       
+        let a = 3.0
+        let b = 5.0
+        let c = -2.0
+        let quad = Quadratic()
+        
+        let form1sol2 = quad.quadraticForm1Sol2(aValue: a, bValue: b, cValue: c)
+        
+        XCTAssertEqual(form1sol2, ((-2.000)), accuracy: 1.0E-7, "was not equal to this resolution")
+    }
+    
+    func testForm2Solution1() async{
+       
+        let a = 3.0
+        let b = 5.0
+        let c = -2.0
+        let quad = Quadratic()
+        
+        let form2sol1 = quad.quadraticForm2Sol1(aValue: a, bValue: b, cValue: c)
+        
+        XCTAssertEqual(form2sol1, ((1/3)), accuracy: 1.0E-7, "was not equal to this resolution")
+    }
+
+    func testForm2Solution2() async{
+       
+        let a = 3.0
+        let b = 5.0
+        let c = -2.0
+        let quad = Quadratic()
+        
+        let form2sol2 = quad.quadraticForm2Sol2(aValue: a, bValue: b, cValue: c)
+        
+        XCTAssertEqual(form2sol2, -2.000, accuracy: 1.0E-7, "was not equal to this resolution")
+    }
+
+    
 
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
